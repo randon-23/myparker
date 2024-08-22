ba94d3fbbd048d8b7fee2f03124890431f36bd3c
randon-23 <arandon24@gmail.com>
2024-08-22 23:21:46 +0200

    removed styles from app.js

--- App.js
+++ App.js
@@ -29,12 +29,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
